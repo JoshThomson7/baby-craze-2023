eef2c6b17d608dcfa280425c0786281474c35fca
JoshThomson7 <josh@fl1.digital>
2023-09-13 21:16:02 +0100

    Style products

--- modules/flexible-content/layouts/fc_products.php
+++ modules/flexible-content/layouts/fc_products.php
@@ -4,11 +4,7 @@
 */
 $prod_box_num = get_sub_field('boxes_per_row');
 
-$product_title = get_sub_field('product_title');
-$product_description = get_sub_field('product_description');
-$product_price = get_sub_field('product_price');
-$product_button_text = get_sub_field('product_button_text');
-$product_button_link = get_sub_field('product_button_link');
+
 
 // $alt_title = get_sub_field('override_page_title');
 ?>
@@ -21,15 +17,25 @@ $product_button_link = get_sub_field('product_button_link');
 
         $attachment_id = get_field('product_image', $product_link->ID);
         $product_link_img = vt_resize( $attachment_id, '', 800, 800, true);
-    ?>
-        <article class="<?php echo $prod_box_num; ?>">
-            <a href="<?php echo get_permalink($product_link->ID); ?>" style="background-image:url(<?php echo $product_link_img['url']; ?>);">
 
-                <div class="grid__link__content">
-                    <h3 class="ts"><?php echo get_the_title($product_link->ID); ?><i class="fa fa-arrow-right"></i></h3>
-                    <a href="" class="button primary"><?php echo $product_button_text; ?></a>
-                </div><!-- grid__link__content -->
-            </a>
-        </article>
+        $product_title = get_field('product_title', $product_link->ID);
+        $product_description = get_field('product_description', $product_link->ID);
+        $product_price = get_field('product_price', $product_link->ID);
+        $product_button_text = get_field('product_button_text', $product_link->ID);
+        $product_button_link = get_field('product_button_link', $product_link->ID);
+    ?>
+            <article class="<?php echo $prod_box_num; ?>">
+                <div class="padder">
+                    
+                    <figure style="background-image:url(<?php echo $product_link_img['url']; ?>);"></figure>
+
+                    <div class="grid-box-content ">
+                        <h3>Title: <?php echo $product_title; ?></h3>
+                        <p>Description: <?php echo $product_description; ?></p>
+                        <p>Price: <?php echo $product_price; ?></p>
+                        <a href="<?php echo $product_button_link; ?>" class="button secondary"><?php echo $product_button_text; ?></a>
+                    </div><!-- grid__box__content -->
+                </div><!-- padder -->
+            </article>
     <?php endforeach; ?>
 </div><!-- grid -->
